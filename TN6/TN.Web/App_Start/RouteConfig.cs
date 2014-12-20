56bbc05cac7d79afbbf91253f778978faca38783
Oleg Shalygin <oshalygin@gmail.com>
2014-12-20 14:02:28 -0800

    Working RouteConfig

--- TN6/TN.Web/App_Start/RouteConfig.cs
+++ TN6/TN.Web/App_Start/RouteConfig.cs
@@ -14,23 +14,26 @@ namespace TN.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
 
+            //Blog/Post
+            routes.MapRoute(
+               name: "BlogTitle",
+               url: "Blog/Post/{UrlTitle}",
+               defaults: new { controller = "Blog", action = "Post" });
+
+            //Blog
+            routes.MapRoute(
+                name: "Blog",
+                url: "Blog",
+                defaults: new { controller = "Blog", action = "Index", id = UrlParameter.Optional });
+
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
 
-
-            routes.MapRoute(
-               name: "BlogTitle",
-               url: "Blog/{UrlTitle}",
-               defaults: new { controller = "Blog", action = "Post"});
-
-
-            routes.MapRoute(
-                name: "Blog",
-                url: "Blog",
-                defaults: new {controller = "Blog", action = "Index", id = UrlParameter.Optional});
+   
         }
     }
 }
