c35207ee56b32ceed2e7f3c07a9a29d7d94e7bec
Oleg Shalygin <oshalygin@gmail.com>
2015-02-07 16:38:14 -0800

    Adding FancyBox Funcationality to Gallarey Page

---
+++ TN6/TN.Web/Controllers/PicturesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TN.Web.Controllers
+{
+    public class PicturesController : Controller
+    {
+        //
+        // GET: /Pictures/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
